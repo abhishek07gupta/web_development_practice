c08af044596c7f42c95df310fe1e848521320f07
Abhishek gupta <07abhishekgupta07@gmail.com>
2022-12-02 22:48:58 +0530

    5th day
    done upto button made to change on click,set correct names and photos of the song to display....stuch on adding song to their name

--- spotify clone/script.js
+++ spotify clone/script.js
@@ -4,16 +4,16 @@ let masterPlay = document.getElementById("masterPlay");
 let myProgressBar = document.getElementById("myProgressBar");
 let gif=document.getElementById("playGif");
 let songs=[
-    {songName: "let me love you" , filePath:"songs/1.mp3" , coverPath: "covers/1.jpg"},
-    {songName: "helo" , filePath:"songs/2.mp3" , coverPath: "covers/2.jpg"},
-    {songName: "let me love you" , filePath:"songs/3.mp3" , coverPath: "covers/3.jpg"},
-    {songName: "let me love you" , filePath:"songs/4.mp3" , coverPath: "covers/4.jpg"},
-    {songName: "let me love you" , filePath:"songs/5.mp3" , coverPath: "covers/5.jpg"},
-    {songName: "let me love you" , filePath:"songs/6.mp3" , coverPath: "covers/6.jpg"},
-    {songName: "let me love you" , filePath:"songs/7.mp3" , coverPath: "covers/7.jpg"},
-    {songName: "let me love you" , filePath:"songs/8.mp3" , coverPath: "covers/8.jpg"},
-    {songName: "let me love you" , filePath:"songs/9.mp3" , coverPath: "covers/9.jpg"},
-    {songName: "let me love you" , filePath:"songs/10.mp3" , coverPath: "covers/10.jpg"}
+    {songName: "Warriyo - Mortals" , filePath:"songs/1.mp3" , coverPath: "covers/1.jpg"},
+    {songName: "Cielo - Huma-Huma" , filePath:"songs/2.mp3" , coverPath: "covers/2.jpg"},
+    {songName: "Deaf Kev - Invincibles" , filePath:"songs/3.mp3" , coverPath: "covers/3.jpg"},
+    {songName: "Different Heaven & Ehide" , filePath:"songs/4.mp3" , coverPath: "covers/4.jpg"},
+    {songName: "Janji - Heroes - Tonight" , filePath:"songs/5.mp3" , coverPath: "covers/5.jpg"},
+    {songName: "Rabba - Salame-ishq" , filePath:"songs/6.mp3" , coverPath: "covers/6.jpg"},
+    {songName: "Wakanda - Marvels" , filePath:"songs/7.mp3" , coverPath: "covers/7.jpg"},
+    {songName: "Undefeatable - Sion" , filePath:"songs/8.mp3" , coverPath: "covers/8.jpg"},
+    {songName: "Love and Thunder - Thor" , filePath:"songs/9.mp3" , coverPath: "covers/9.jpg"},
+    {songName: "Courtesy call - Thousand" , filePath:"songs/10.mp3" , coverPath: "covers/10.jpg"}
 ]
 let songItems=Array.from(document.getElementsByClassName('songItem'));
 songItems.forEach((element,i)=>{
@@ -43,4 +43,27 @@ audioElement.addEventListener('timeupdate' , ()=>{
 
 myProgressBar.addEventListener('change' , ()=>{
     audioElement.currentTime=myProgressBar.value*audioElement.duration/100;
+})
+
+function makeAllPlays() {
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+        element.classList.remove('fa-circle-pause');
+        element.classList.add('fa-circle-play');
+    })
+}
+
+Array.from(document.getElementsByClassName('songItemPlay')).forEach(element =>{
+    element.addEventListener('click', e=>{
+        makeAllPlays();
+        songIndex=parseInt(e.target.id);
+        console.log(songIndex);
+        audioElement.src="songs/${songIndex}.mp3";
+        e.target.classList.remove('fa-circle-play');
+        e.target.classList.add('fa-circle-pause');
+        masterPlay.classList.remove('fa-circle-play');
+        masterPlay.classList.add('fa-circle-pause');
+        gif.style.opacity=1;
+        audioElement.currentTime=0;
+        audioElement.play();
+    })
 })
